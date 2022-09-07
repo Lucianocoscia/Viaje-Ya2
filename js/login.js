2f4f81b219a6e36883e5f19c29ecc349420b4b86
Luciano Coscia <lucianocoscia5@gmail.com>
2022-09-06 23:14:53 -0300

    Solucionando href

--- js/login.js
+++ js/login.js
@@ -97,7 +97,7 @@ function loguearte (){
                 title: `Bienvenido ${localStorage.nombre}`,
                 showConfirmButton: false,
                 timer: 1500
-            }), setTimeout(function(){window.location.href = 'index.html#contenedorReserva'}, 2000);  ;
+            }), setTimeout(function(){window.location.href = '/index.html#contenedorReserva'}, 2000);  ;
 
         email2.value = "";
         contrasenia2.value ="";
